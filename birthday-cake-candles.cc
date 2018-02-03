cbef9723c3cac3bc95b99e7076894f083459252d
kantuni <henrikh.kantuni@gmail.com>
2018-02-03 19:00:11 +0400

    Birthday Cake Candles

---
+++ birthday-cake-candles.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int birthdayCakeCandles(int n, vector<int> candles) {
+  sort(candles.begin(), candles.end(), greater<int>());
+  int count = 1;
+  for (int i = 1; i < n; i++) {
+    if (candles[i] == candles[0]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> candles(n);
+  for (int i = 0; i < n; i++) {
+     cin >> candles[i];
+  }
+  int result = birthdayCakeCandles(n, candles);
+  cout << result << "\n";
+  return 0;
+}
