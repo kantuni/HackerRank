2aa6c25da3a4c426455eeca9e173f7f0c0bc6743
kantuni <henrikh.kantuni@gmail.com>
2018-02-07 16:48:09 +0400

    Kangaroo

---
+++ kangaroo.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void kangaroo(int x1, int v1, int x2, int v2) {
+  if (v1 == v2) {
+    cout << (x1 == x2 ? "YES\n" : "NO\n");
+  } else {
+    double t = 1.0 * (x2 - x1) / (v1 - v2);
+    cout << (t >= 0 and round(t) == t ? "YES\n" : "NO\n");
+  }
+}
+
+int main() {
+  int x1, v1, x2, v2;
+  cin >> x1 >> v1 >> x2 >> v2;
+  kangaroo(x1, v1, x2, v2);
+  return 0;
+}
