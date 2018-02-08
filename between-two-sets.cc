26fe141cd425d71941ec0c550b3f4f73282d8935
kantuni <henrikh.kantuni@gmail.com>
2018-02-08 22:03:55 +0400

    Between Two Sets

---
+++ between-two-sets.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> A(n), B(m);
+  
+  for (int i = 0; i < n; i++) {
+    cin >> A[i];
+  }
+  
+  for (int i = 0; i < m; i++) {
+    cin >> B[i];
+  }
+  
+  int counter = 0;
+  for (int i = 1; i <= 100; i++) {
+    bool ok = true;
+    for (int j = 0; j < n; j++) {
+      if (i % A[j] != 0) {
+        ok = false;
+        break;
+      }
+    }
+    for (int j = 0; j < m; j++) {
+      if (B[j] % i != 0) {
+        ok = false;
+        break;
+      }
+    }
+    if (ok) counter++;
+  }
+  
+  cout << counter << "\n";
+  return 0;
+}
