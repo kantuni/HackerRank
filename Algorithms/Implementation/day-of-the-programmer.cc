4c80b3a5d7b3089fff01efcbeeffb219bb224103
kantuni <henrikh.kantuni@gmail.com>
2018-02-28 09:15:49 -0500

    Update day-of-the-programmer.cc

--- Algorithms/Implementation/day-of-the-programmer.cc
+++ Algorithms/Implementation/day-of-the-programmer.cc
@@ -2,20 +2,21 @@
 using namespace std;
 
 int main() {
-  int d, y;
+  int y;
   cin >> y;
   
+  int d = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+  bool leap = false;
+
   if (y < 1918) {
-    d = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
-    if (y % 4 == 0) d++;
+    leap = (y % 4 == 0);
   } else if (y == 1918) {
-    d = 32 + 14 + 31 + 30 + 31 + 30 + 31 + 31;
+    d -= 13;
   } else {
-    d = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
-    if (y % 400 == 0) d++;
-    else if (y % 4 == 0 and y % 100 != 0) d++;
+    leap = (y % 400 == 0) or (y % 4 == 0 and y % 100 != 0);
   }
   
+  if (leap) d++;
   printf("%02d.09.%d\n", 256 - d, y);
   return 0;
 }
