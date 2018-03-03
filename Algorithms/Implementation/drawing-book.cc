4f6bbdd0c9760558f360d2d309ead52c10cdd288
kantuni <henrikh.kantuni@gmail.com>
2018-03-03 16:21:23 -0500

    Drawing Book

---
+++ Algorithms/Implementation/drawing-book.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, p;
+  cin >> n >> p;
+  int ff = p / 2;
+  int fb = n / 2 - ff;
+  cout << min(ff, fb) << "\n";
+  return 0;
+}
