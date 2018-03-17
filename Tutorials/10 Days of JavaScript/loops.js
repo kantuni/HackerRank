6a6b852ced578b14836bbe6d3131f74229ab4e3c
kantuni <henrikh.kantuni@gmail.com>
2018-03-17 18:10:56 -0400

    More elegant JavaScript

--- Tutorials/10 Days of JavaScript/loops.js
+++ Tutorials/10 Days of JavaScript/loops.js
@@ -3,14 +3,9 @@ function isVowel(letter) {
 }
 
 function vowelsAndConsonants(s) {
-  let vowels = [], consonants = [];
-  for (const letter of s) {
-    if (isVowel(letter)) {
-      vowels.push(letter);
-    } else {
-      consonants.push(letter);
-    }
-  }
+  const letters = [...s];
+  const vowels = letters.filter(letter => isVowel(letter));
+  const consonants = letters.filter(letter => !isVowel(letter));
   vowels.map(vowel => console.log(vowel));
   consonants.map(consonant => console.log(consonant));
 }
