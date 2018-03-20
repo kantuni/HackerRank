3695e6517de24be800ce9a2c10caed40d7ebb42c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 20:34:32 -0400

    Update time-conversion.cc

--- Algorithms/Warmup/time-conversion.cc
+++ Algorithms/Warmup/time-conversion.cc
@@ -3,9 +3,14 @@ using namespace std;
 
 int main() {
   string hh, mm, ss, ampm;
-  char h1, h2; cin >> h1 >> h2; cin.ignore();
-  char m1, m2; cin >> m1 >> m2; cin.ignore();
-  char s1, s2; cin >> s1 >> s2 >> ampm;
+  char h1, h2;
+  cin >> h1 >> h2;
+  cin.ignore();
+  char m1, m2;
+  cin >> m1 >> m2;
+  cin.ignore();
+  char s1, s2;
+  cin >> s1 >> s2 >> ampm;
   hh = string(1, h1) + string(1, h2);
   mm = string(1, m1) + string(1, m2);
   ss = string(1, s1) + string(1, s2);
