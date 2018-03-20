4be49bdaf802265dacd83207dc068233e32182f7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 20:28:34 -0400

    Update birthday-cake-candles.cc

--- Algorithms/Warmup/birthday-cake-candles.cc
+++ Algorithms/Warmup/birthday-cake-candles.cc
@@ -5,9 +5,7 @@ int birthdayCakeCandles(int n, vector<int> candles) {
   sort(candles.rbegin(), candles.rend());
   int count = 1;
   for (int i = 1; i < n; i++) {
-    if (candles[i] == candles[0]) {
-      count++;
-    }
+    if (candles[i] == candles[0]) count++;
   }
   return count;
 }
@@ -16,10 +14,7 @@ int main() {
   int n;
   cin >> n;
   vector<int> candles(n);
-  for (int i = 0; i < n; i++) {
-     cin >> candles[i];
-  }
-  int result = birthdayCakeCandles(n, candles);
-  cout << result << "\n";
+  for (int i = 0; i < n; i++) cin >> candles[i];
+  cout << birthdayCakeCandles(n, candles) << "\n";
   return 0;
 }
