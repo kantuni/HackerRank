ef5465135f4473dcc31cc6594632c11cda0297e7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 20:31:15 -0400

    Update diagonal-difference.cc

--- Algorithms/Warmup/diagonal-difference.cc
+++ Algorithms/Warmup/diagonal-difference.cc
@@ -1,26 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-
 int main() {
   int n;
   cin >> n;
-  
-  vvi M(n, vi(n));
+  vector<vector<int> > M(n, vector<int>(n));
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      cin >> M[i][j];
-    }
+    for (int j = 0; j < n; j++) cin >> M[i][j];
   }
-  
   int ld = 0, rd = 0;
   for (int i = 0; i < n; i++) {
     ld += M[i][i];
     rd += M[i][n - i - 1];
   }
-  
   cout << abs(ld - rd) << "\n";
   return 0;
 }
