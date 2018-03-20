9a3bb6bd8f6296db20360c0b35bbedd6cb7396ec
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-20 16:34:57 -0400

    Update throw.js

--- Tutorials/10 Days of JavaScript/throw.js
+++ Tutorials/10 Days of JavaScript/throw.js
@@ -1,5 +1,9 @@
 function isPositive(a) {
-  if (a > 0) return 'YES';
-  else if (a < 0) throw new Error('Negative Error');
-  else throw new Error('Zero Error');
+  if (a > 0) {
+    return 'YES';
+  } else if (a < 0) {
+    throw new Error('Negative Error');
+  } else {
+    throw new Error('Zero Error');
+  }
 }
