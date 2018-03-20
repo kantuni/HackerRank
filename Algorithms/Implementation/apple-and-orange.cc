142b52cf06d6c06cda8d3c722d261af41f465c06
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-20 16:09:13 -0400

    Update apple-and-orange.cc

--- Algorithms/Implementation/apple-and-orange.cc
+++ Algorithms/Implementation/apple-and-orange.cc
@@ -2,10 +2,10 @@
 using namespace std;
 
 void countApplesAndOranges(int s, int t, int a, int b, vector<int> apples, vector<int> oranges) {
-  int capples = count_if(apples.begin(), apples.end(), [a, s, t] (int d) {
+  int capples = count_if(apples.begin(), apples.end(), [s, t, a] (int d) {
     return a + d >= s and a + d <= t;
   });
-  int coranges = count_if(oranges.begin(), oranges.end(), [b, s, t] (int d) { 
+  int coranges = count_if(oranges.begin(), oranges.end(), [s, t, b] (int d) { 
     return b + d >= s and b + d <= t;
   });
   cout << capples << "\n" << coranges << "\n";
@@ -15,13 +15,9 @@ int main() {
   int s, t, a, b, m, n;
   cin >> s >> t >> a >> b >> m >> n;
   vector<int> apples(m);
-  for (int i = 0; i < m; i++) {
-    cin >> apples[i];
-  }
+  for (int i = 0; i < m; i++) cin >> apples[i];
   vector<int> oranges(n);
-  for (int i = 0; i < n; i++) {
-    cin >> oranges[i];
-  }
+  for (int i = 0; i < n; i++) cin >> oranges[i];
   countApplesAndOranges(s, t, a, b, apples, oranges);
   return 0;
 }
