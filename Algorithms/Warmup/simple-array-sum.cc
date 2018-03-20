543a8abfbfad58c2a1d87202c904fed178074fcf
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 20:32:45 -0400

    Update simple-array-sum.cc

--- Algorithms/Warmup/simple-array-sum.cc
+++ Algorithms/Warmup/simple-array-sum.cc
@@ -3,22 +3,15 @@ using namespace std;
 
 int simpleArraySum(int n, vector<int> array) {
   int sum = 0;
-  for (int i = 0; i < n; i++) {
-    sum += array[i];
-  }
+  for (int i = 0; i < n; i++) sum += array[i];
   return sum;
 }
 
 int main() {
   int n;
   cin >> n;
-
   vector<int> array(n);
-  for (int i = 0; i < n; i++) {
-     cin >> array[i];
-  }
-  
-  int result = simpleArraySum(n, array);
-  cout << result << "\n";
+  for (int i = 0; i < n; i++) cin >> array[i];
+  cout << simpleArraySum(n, array) << "\n";
   return 0;
 }
