aa829be83151cc18d2b32de7231d15228918c667
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 20:29:48 -0400

    Update compare-the-triplets.cc

--- Algorithms/Warmup/compare-the-triplets.cc
+++ Algorithms/Warmup/compare-the-triplets.cc
@@ -6,27 +6,13 @@ int main() {
   cin >> a0 >> a1 >> a2;
   int b0, b1, b2;
   cin >> b0 >> b1 >> b2;
-  
   int a = 0, b = 0;
-  
-  if (a0 > b0) {
-    a += 1;
-  } else if (a0 < b0) {
-    b += 1;
-  }
-  
-  if (a1 > b1) {
-    a += 1;
-  } else if (a1 < b1) {
-    b += 1;
-  }
-  
-  if (a2 > b2) {
-    a += 1;
-  } else if (a2 < b2) {
-    b += 1;
-  }
-  
+  if (a0 > b0) a += 1;
+  else if (a0 < b0) b += 1;
+  if (a1 > b1) a += 1;
+  else if (a1 < b1) b += 1;
+  if (a2 > b2) a += 1;
+  else if (a2 < b2) b += 1;
   cout << a << " " << b << "\n";
   return 0;
 }
