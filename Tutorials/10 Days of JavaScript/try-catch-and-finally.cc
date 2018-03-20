bd79d1c04a40a73fec23c1b62b2e144955df7665
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-20 16:37:57 -0400

    Update try-catch-and-finally.cc

--- Tutorials/10 Days of JavaScript/try-catch-and-finally.cc
+++ Tutorials/10 Days of JavaScript/try-catch-and-finally.cc
@@ -1,9 +1,5 @@
 function reverseString(s) {
-  try {
-    s = s.split('').reverse().join('');
-  } catch (e) {
-    console.log(e.message);
-  } finally {
-    console.log(s);
-  }
+  try s = s.split('').reverse().join('');
+  catch (e) console.log(e.message);
+  finally console.log(s);
 }
