2ed9711a149de6ebd83df9917fe59ef0af421416
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-20 16:16:20 -0400

    Update kangaroo.cc

--- Algorithms/Implementation/kangaroo.cc
+++ Algorithms/Implementation/kangaroo.cc
@@ -2,9 +2,8 @@
 using namespace std;
 
 void kangaroo(int x1, int v1, int x2, int v2) {
-  if (v1 == v2) {
-    cout << (x1 == x2 ? "YES\n" : "NO\n");
-  } else {
+  if (v1 == v2) cout << (x1 == x2 ? "YES\n" : "NO\n");
+  else {
     double t = 1.0 * (x2 - x1) / (v1 - v2);
     cout << (t >= 0 and round(t) == t ? "YES\n" : "NO\n");
   }
