6959d30ef889a1ff75b9e3190d7fb7774a066608
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 20:33:52 -0400

    Update staircase.cc

--- Algorithms/Warmup/staircase.cc
+++ Algorithms/Warmup/staircase.cc
@@ -1,18 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<char> vc;
-typedef vector<vc> vvc;
-
 int main() {
   int n;
   cin >> n;
-  vvc M(n, vc(n, ' '));
+  vector<vector<char> > M(n, vector<char>(n, ' '));
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
-      if (i + j >= n - 1) {
-        M[i][j] = '#';
-      }
+      if (i + j >= n - 1) M[i][j] = '#';
       cout << M[i][j];
     }
     cout << "\n";
