89b6241365667e04071032aaa468585dfa745c73
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-20 16:15:52 -0400

    Update grading.cc

--- Algorithms/Implementation/grading.cc
+++ Algorithms/Implementation/grading.cc
@@ -4,9 +4,7 @@ using namespace std;
 vector<int> solve(vector<int> grades) {
   for (int i = 0; i < grades.size(); i++) {
     int next = (grades[i] / 5 + 1) * 5;
-    if (next - grades[i] < 3 && grades[i] >= 38) {
-      grades[i] = next;
-    }
+    if (next - grades[i] < 3 and grades[i] >= 38) grades[i] = next;
   }
   return grades;
 }
@@ -14,12 +12,8 @@ vector<int> solve(vector<int> grades) {
 int main() {
   int n;
   cin >> n;
-  
   vector<int> grades(n);
-  for (int i = 0; i < n; i++) {
-    cin >> grades[i];
-  }
-  
+  for (int i = 0; i < n; i++) cin >> grades[i];
   vector<int> result = solve(grades);
   for (int i = 0; i < result.size(); i++) {
     cout << result[i] << (i != result.size() - 1 ? "\n" : "");
