731ae5fdb40647c7ba62cbe518b5b23eeb4b90ea
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-20 16:11:56 -0400

    Update divisible-sum-pairs.cc

--- Algorithms/Implementation/divisible-sum-pairs.cc
+++ Algorithms/Implementation/divisible-sum-pairs.cc
@@ -5,19 +5,13 @@ int main() {
   int n, k;
   cin >> n >> k;
   vector<int> a(n);
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
-  }
-  
+  for (int i = 0; i < n; i++) cin >> a[i];
   int cnt = 0;
   for (int i = 0; i < n - 1; i++) {
     for (int j = i + 1; j < n; j++) {
-      if ((a[i] + a[j]) % k == 0) {
-        cnt++;
-      }
+      if ((a[i] + a[j]) % k == 0) cnt++;
     }
   }
-  
   cout << cnt << "\n";
   return 0;
 }
