5f221153bd10a37bba3a53f100ebedb05849f89c
kantuni <henrikh.kantuni@gmail.com>
2018-03-23 18:45:59 -0400

    Update button.js

--- Tutorials/10 Days of JavaScript/Create a Button/js/button.js
+++ Tutorials/10 Days of JavaScript/Create a Button/js/button.js
@@ -1,11 +1,11 @@
 const button = document.createElement('button');
 Object.assign(button, {
   id: 'btn',
-  innerText: '0'
+  textContent: '0'
 });
 document.body.appendChild(button);
 
 button.addEventListener('click', e => {
-  const value = Number(button.innerText);
-  button.innerText = String(value + 1);
+  const value = Number(button.textContent);
+  button.textContent = String(value + 1);
 });
