d559611926cdc0a91b3b2f509af1cd9e0ae8fd0a
kantuni <henrikh.kantuni@gmail.com>
2018-03-24 18:37:58 -0400

    Counting Valleys

---
+++ Skills/Algorithms/Implementation/counting-valleys.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  int cnt = 0, lvl = 0;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == 'D') {
+      if (lvl == 0) cnt++;
+      lvl--;
+    }
+    else lvl++;
+  }
+  cout << cnt << "\n";
+  return 0;
+}
