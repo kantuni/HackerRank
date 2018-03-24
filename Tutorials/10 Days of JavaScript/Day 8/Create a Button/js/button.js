0e743dbcd711c1e9fb3860dec1a75817c1c2a5f4
kantuni <henrikh.kantuni@gmail.com>
2018-03-24 08:34:27 -0400

    IIFE

--- Tutorials/10 Days of JavaScript/Day 8/Create a Button/js/button.js
+++ Tutorials/10 Days of JavaScript/Day 8/Create a Button/js/button.js
@@ -1,11 +1,13 @@
-const button = document.createElement('button');
-Object.assign(button, {
-  id: 'btn',
-  textContent: '0'
-});
-document.body.appendChild(button);
+(function () {
+  const button = document.createElement('button');
+  Object.assign(button, {
+    id: 'btn',
+    textContent: '0'
+  });
+  document.body.appendChild(button);
 
-button.addEventListener('click', e => {
-  const value = Number(button.textContent);
-  button.textContent = String(value + 1);
-});
+  button.addEventListener('click', e => {
+    const value = Number(button.textContent);
+    button.textContent = String(value + 1);
+  });
+})();
