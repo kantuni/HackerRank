829b634198a791f66e8b384747d7ef9db6f64d5b
kantuni <henrikh.kantuni@gmail.com>
2018-03-24 08:37:19 -0400

    Day 9: Binary Calculator

---
+++ Tutorials/10 Days of JavaScript/Day 9/Binary Calculator/js/binary-calculator.js
@@ -0,0 +1,39 @@
+(function () {
+  const res = document.getElementById('res');
+
+  document.getElementById('btn0').addEventListener('click', e => {
+    res.textContent += '0';
+  });
+
+  document.getElementById('btn1').addEventListener('click', e => {
+    res.textContent += '1';
+  });
+
+  document.getElementById('btnClr').addEventListener('click', e => {
+    res.textContent = '';
+  });
+
+  document.getElementById('btnEql').addEventListener('click', e => {
+    const re = /(\d+)([\+\-\*\/])(\d+)/;
+    let [_, operand1, operator, operand2] = res.textContent.match(re);
+    operand1 = parseInt(operand1, 2);
+    operand2 = parseInt(operand2, 2);
+    res.textContent = Math.floor(eval(operand1 + operator + operand2)).toString(2);
+  });
+
+  document.getElementById('btnSum').addEventListener('click', e => {
+    res.textContent += '+';
+  });
+
+  document.getElementById('btnSub').addEventListener('click', e => {
+    res.textContent += '-';
+  });
+
+  document.getElementById('btnMul').addEventListener('click', e => {
+    res.textContent += '*';
+  });
+
+  document.getElementById('btnDiv').addEventListener('click', e => {
+    res.textContent += '/';
+  });
+})();
