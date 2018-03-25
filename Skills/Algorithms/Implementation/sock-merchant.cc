893587ff978045e32caade96c751bede51ebb60e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-25 10:47:28 -0400

    Update sock-merchant.cc

--- Skills/Algorithms/Implementation/sock-merchant.cc
+++ Skills/Algorithms/Implementation/sock-merchant.cc
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main() {
-  unordered_map<int, int> um;
+  unordered_map<int, int> memo;
   int n;
   cin >> n;
   while (n--) {
     int c;
     cin >> c;
-    um[c]++;
+    memo[c]++;
   }
   int cnt = 0;
-  for (auto kv: um) cnt += kv.second / 2;
+  for (auto kv: memo) cnt += kv.second / 2;
   cout << cnt << "\n";
   return 0;
 }
