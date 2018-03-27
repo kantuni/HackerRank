b66825c047b344d2adccefe8390154692f273c77
kantuni <henrikh.kantuni@gmail.com>
2018-03-26 22:41:28 -0400

    Update picking-numbers.cc

--- Algorithms/Implementation/picking-numbers.cc
+++ Algorithms/Implementation/picking-numbers.cc
@@ -4,23 +4,15 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  vector<int> a(n);
-  for (int i = 0; i < n; i++) cin >> a[i];
-  sort(a.begin(), a.end());
-  int cnt = 1, maxcnt = 1;
-  bool more = true;
-  for (int i = 0; i < n - 1; i++) {
-    if (a[i + 1] == a[i]) cnt++;
-    else if (more and a[i + 1] - a[i] == 1) {
-      more = false;
-      cnt++;
-    }
-    else {
-      more = true;
-      cnt = 1;
-    }
-    maxcnt = max(cnt, maxcnt);
+  vector<int> a(n), freq(100, 0);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    freq[a[i]]++;
   }
-  cout << maxcnt << "\n";
+  int cnt = 1;
+  for (int i = 0; i < n - 1; i++) {
+    cnt = max(cnt, freq[i] + freq[i + 1]);
+  }
+  cout << cnt << "\n";
   return 0;
 }
