b9d68f6d88cf20ce9fbde60d7091e653323df0f4
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 16:23:30 +0400

    Update breaking-best-and-worst-records.cpp

--- Algorithms/Implementation/breaking-best-and-worst-records.cpp
+++ Algorithms/Implementation/breaking-best-and-worst-records.cpp
@@ -5,7 +5,9 @@ int main() {
   int n;
   cin >> n;
   vector<int> s(n);
-  for (int i = 0; i < n; i++) cin >> s[i];
+  for (int i = 0; i < n; i++) {
+    cin >> s[i];
+  }
   int min = s[0], max = s[0];
   int cmin = 0, cmax = 0;
   for (int i = 1; i < n; i++) {
