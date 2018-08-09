1828532e27b32e1acb4642d9c3f8dfcf285e2e20
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 17:00:26 +0400

    Update the-birthday-bar.cpp

--- Algorithms/Implementation/the-birthday-bar.cpp
+++ Algorithms/Implementation/the-birthday-bar.cpp
@@ -5,14 +5,20 @@ int main() {
   int n; 
   cin >> n;
   vector<int> s(n);
-  for (int i = 0; i < n; i++) cin >> s[i];
+  for (int i = 0; i < n; i++) {
+    cin >> s[i];
+  }
   int d, m;
   cin >> d >> m;
   int cnt = 0;
   for (int i = 0; i <= n - m; i++) {
     int sum = 0;
-    for (int j = 0; j < m; j++) sum += s[i + j];
-    if (sum == d) cnt++;
+    for (int j = 0; j < m; j++) {
+      sum += s[i + j];
+    }
+    if (sum == d) {
+      cnt++;
+    }
   }
   cout << cnt << "\n";
   return 0;
