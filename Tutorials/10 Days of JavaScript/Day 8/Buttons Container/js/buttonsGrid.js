097edae5df46e629f851a58ecfc6618178c00917
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 17:15:16 +0400

    Update buttonsGrid.js

--- Tutorials/10 Days of JavaScript/Day 8/Buttons Container/js/buttonsGrid.js
+++ Tutorials/10 Days of JavaScript/Day 8/Buttons Container/js/buttonsGrid.js
@@ -1,10 +1,10 @@
 (function () {
-  document.getElementById('btn5').addEventListener('click', e => {
-    const buttons = [...document.querySelectorAll('#btns button')];
-    const offset = [3, -1, -1, 3, 0, -3, 1, 1, -3];
+  document.getElementById("btn5").addEventListener("click", e => {
+    const buttons = [...document.querySelectorAll("#btns button")]
+    const offset = [3, -1, -1, 3, 0, -3, 1, 1, -3]
     for (button of buttons) {
-      const index = Number(button.textContent);
-      button.textContent = (index + offset[index - 1]).toString();
+      const index = Number(button.textContent)
+      button.textContent = (index + offset[index - 1]).toString()
     }
-  });
-})();
+  })
+})()
