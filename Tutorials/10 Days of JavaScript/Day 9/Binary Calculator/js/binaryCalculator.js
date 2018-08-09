a14f2c8d7286b9896d4556a592c5d5897a4b64ad
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 17:19:04 +0400

    Update binaryCalculator.js

--- Tutorials/10 Days of JavaScript/Day 9/Binary Calculator/js/binaryCalculator.js
+++ Tutorials/10 Days of JavaScript/Day 9/Binary Calculator/js/binaryCalculator.js
@@ -1,38 +1,38 @@
 (function () {
-  document.getElementById('btn0').addEventListener('click', e => {
-    document.getElementById('res').textContent += '0';
-  });
+  document.getElementById("btn0").addEventListener("click", e => {
+    document.getElementById("res").textContent += "0"
+  })
 
-  document.getElementById('btn1').addEventListener('click', e => {
-    document.getElementById('res').textContent += '1';
-  });
+  document.getElementById("btn1").addEventListener("click", e => {
+    document.getElementById("res").textContent += "1"
+  })
 
-  document.getElementById('btnClr').addEventListener('click', e => {
-    document.getElementById('res').textContent = '';
-  });
+  document.getElementById("btnClr").addEventListener("click", e => {
+    document.getElementById("res").textContent = ""
+  })
 
-  document.getElementById('btnEql').addEventListener('click', e => {
-    const res = document.getElementById('res');
-    const re = /(\d+)([\+\-\*\/])(\d+)/;
-    let [_, operand1, operator, operand2] = res.textContent.match(re);
-    operand1 = parseInt(operand1, 2);
-    operand2 = parseInt(operand2, 2);
-    res.textContent = Math.floor(eval(operand1 + operator + operand2)).toString(2);
-  });
+  document.getElementById("btnEql").addEventListener("click", e => {
+    const res = document.getElementById("res")
+    const re = /(\d+)([\+\-\*\/])(\d+)/
+    let [_, operand1, operator, operand2] = res.textContent.match(re)
+    operand1 = parseInt(operand1, 2)
+    operand2 = parseInt(operand2, 2)
+    res.textContent = Math.floor(eval(operand1 + operator + operand2)).toString(2)
+  })
 
-  document.getElementById('btnSum').addEventListener('click', e => {
-    document.getElementById('res').textContent += '+';
-  });
+  document.getElementById("btnSum").addEventListener("click", e => {
+    document.getElementById("res").textContent += "+"
+  })
 
-  document.getElementById('btnSub').addEventListener('click', e => {
-    document.getElementById('res').textContent += '-';
-  });
+  document.getElementById("btnSub").addEventListener("click", e => {
+    document.getElementById("res").textContent += "-"
+  })
 
-  document.getElementById('btnMul').addEventListener('click', e => {
-    document.getElementById('res').textContent += '*';
-  });
+  document.getElementById("btnMul").addEventListener("click", e => {
+    document.getElementById("res").textContent += "*"
+  })
 
-  document.getElementById('btnDiv').addEventListener('click', e => {
-    document.getElementById('res').textContent += '/';
-  });
-})();
+  document.getElementById("btnDiv").addEventListener("click", e => {
+    document.getElementById("res").textContent += "/"
+  })
+})()
