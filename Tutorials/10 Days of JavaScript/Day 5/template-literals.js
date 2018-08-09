54776718c80083ad47687371192dcb58b10319c2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 17:11:36 +0400

    Update template-literals.js

--- Tutorials/10 Days of JavaScript/Day 5/template-literals.js
+++ Tutorials/10 Days of JavaScript/Day 5/template-literals.js
@@ -1,6 +1,6 @@
 function sides(literals, ...expressions) {
-  const [A, P] = expressions;
-  const s1 = (P + Math.sqrt(P * P - 16 * A)) / 4;
-  const s2 = (P - Math.sqrt(P * P - 16 * A)) / 4;
+  const [A, P] = expressions
+  const s1 = (P + Math.sqrt(P * P - 16 * A)) / 4
+  const s2 = (P - Math.sqrt(P * P - 16 * A)) / 4
   return [s1, s2].sort();
 }
