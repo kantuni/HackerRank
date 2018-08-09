59d77447dfecd6353241198573d7a074b21e5841
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 16:24:29 +0400

    Update day-of-the-programmer.cpp

--- Algorithms/Implementation/day-of-the-programmer.cpp
+++ Algorithms/Implementation/day-of-the-programmer.cpp
@@ -6,10 +6,16 @@ int main() {
   cin >> y;
   int d = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
   bool leap = false;
-  if (y < 1918) leap = (y % 4 == 0);
-  else if (y == 1918) d -= 13;
-  else leap = (y % 400 == 0) or (y % 4 == 0 and y % 100 != 0);
-  if (leap) d++;
+  if (y < 1918) {
+    leap = (y % 4 == 0);
+  } else if (y == 1918) {
+    d -= 13;
+  } else {
+    leap = (y % 400 == 0) or (y % 4 == 0 and y % 100 != 0);
+  }
+  if (leap) {
+    d++;
+  }
   printf("%02d.09.%d\n", 256 - d, y);
   return 0;
 }
