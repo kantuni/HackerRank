7627bd53df632e90046303982ee54908328af675
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 15:33:07 +0400

    Update staircase.cpp

--- Algorithms/Warmup/staircase.cpp
+++ Algorithms/Warmup/staircase.cpp
@@ -7,7 +7,9 @@ int main() {
   vector<vector<char> > M(n, vector<char>(n, ' '));
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
-      if (i + j >= n - 1) M[i][j] = '#';
+      if (i + j >= n - 1) {
+        M[i][j] = '#';
+      }
       cout << M[i][j];
     }
     cout << "\n";
