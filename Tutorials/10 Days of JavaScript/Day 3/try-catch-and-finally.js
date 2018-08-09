fd06e8d994e83b5e4275db8ca188d43ee8c04256
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 17:10:02 +0400

    Update try-catch-and-finally.js

--- Tutorials/10 Days of JavaScript/Day 3/try-catch-and-finally.js
+++ Tutorials/10 Days of JavaScript/Day 3/try-catch-and-finally.js
@@ -1,9 +1,9 @@
 function reverseString(s) {
   try {
-    s = s.split('').reverse().join('');
+    s = s.split("").reverse().join("")
   } catch (e) {
-    console.log(e.message);
+    console.log(e.message)
   } finally {
-    console.log(s);
+    console.log(s)
   }
 }
