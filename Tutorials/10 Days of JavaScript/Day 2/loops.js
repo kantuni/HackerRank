a3f3774e9a37f2e6bb0ef1f30df118fb6898aed5
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 17:07:26 +0400

    Update loops.js

--- Tutorials/10 Days of JavaScript/Day 2/loops.js
+++ Tutorials/10 Days of JavaScript/Day 2/loops.js
@@ -1,11 +1,11 @@
 function isVowel(letter) {
-  return /[aeiou]/i.test(letter);
+  return /[aeiou]/i.test(letter)
 }
 
 function vowelsAndConsonants(s) {
-  const letters = [...s];
-  const vowels = letters.filter(letter => isVowel(letter));
-  const consonants = letters.filter(letter => !isVowel(letter));
-  vowels.map(vowel => console.log(vowel));
-  consonants.map(consonant => console.log(consonant));
+  const letters = [...s]
+  const vowels = letters.filter(letter => isVowel(letter))
+  const consonants = letters.filter(letter => !isVowel(letter))
+  vowels.map(vowel => console.log(vowel))
+  consonants.map(consonant => console.log(consonant))
 }
