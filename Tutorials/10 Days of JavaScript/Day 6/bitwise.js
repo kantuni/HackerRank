a2b8b5133f0616fe2198408e0d43bd7d2c4adee0
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 17:12:03 +0400

    Update bitwise.js

--- Tutorials/10 Days of JavaScript/Day 6/bitwise.js
+++ Tutorials/10 Days of JavaScript/Day 6/bitwise.js
@@ -1,8 +1,10 @@
 function getMaxLessThanK(n, k) {
-  let max = 0;
+  let max = 0
   for (let a = 1; a <= n; a++) {
     for (let b = a + 1; b <= n; b++) {
-      if ((a & b) < k) max = Math.max(max, a & b);
+      if ((a & b) < k) {
+        max = Math.max(max, a & b)
+      }
     }
   }
   return max;
