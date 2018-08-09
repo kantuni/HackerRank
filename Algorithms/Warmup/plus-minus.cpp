01ef2b1c20723e927b64b905fe398bba33a24cb1
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 15:31:59 +0400

    Update plus-minus.cpp

--- Algorithms/Warmup/plus-minus.cpp
+++ Algorithms/Warmup/plus-minus.cpp
@@ -8,9 +8,13 @@ int main() {
   for (int i = 0; i < n; i++) {
     int a;
     cin >> a;
-    if (a > 0) pos++;
-    else if (a < 0) neg++;
-    else zero++;
+    if (a > 0) {
+      pos++;
+    } else if (a < 0) {
+      neg++;
+    } else {
+      zero++;
+    }
     total++;
   }
   printf("%.6f\n", 1.0 * pos / total);
