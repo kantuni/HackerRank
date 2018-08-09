7cabffe885c7883e6abef2f731ffadd3c9ef9a8e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 17:03:27 +0400

    Update data-types.js

--- Tutorials/10 Days of JavaScript/Day 0/data-types.js
+++ Tutorials/10 Days of JavaScript/Day 0/data-types.js
@@ -1,7 +1,7 @@
 function performOperation(secondInteger, secondDecimal, secondString) {
   const firstInteger = 4
   const firstDecimal = 4.0
-  const firstString = 'HackerRank '
+  const firstString = "HackerRank "
   console.log(firstInteger + Number(secondInteger))
   console.log(firstDecimal + Number(secondDecimal))
   console.log(firstString + secondString)
