c41e009ad78c213616c76c0c524d5d45d3736238
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-09 16:23:55 +0400

    Update counting-valleys.cpp

--- Algorithms/Implementation/counting-valleys.cpp
+++ Algorithms/Implementation/counting-valleys.cpp
@@ -9,10 +9,13 @@ int main() {
   int cnt = 0, lvl = 0;
   for (int i = 0; i < n; i++) {
     if (s[i] == 'D') {
-      if (lvl == 0) cnt++;
+      if (lvl == 0) {
+        cnt++;
+      }
       lvl--;
+    } else {
+      lvl++;
     }
-    else lvl++;
   }
   cout << cnt << "\n";
   return 0;
