9c5ae12742e659985e0ed8b2501ff1cef88689de
kantuni <henrikh.kantuni@gmail.com>
2018-10-19 10:55:10 -0400

    String Similarity - TLE

---
+++ Algorithms/Strings/string-similarity.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int similarity(string s) {
+  int ans = s.size();
+  for (int i = 1; i < s.size(); i++) {
+    for (int j = i; j < s.size(); j++) {
+      if (s[j] == s[j - i]) {
+        ans++;
+        continue;
+      }
+      break;
+    }
+  }
+  return ans;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    cout << similarity(s) << "\n";
+  }
+  return 0;
+}
