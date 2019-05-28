fb20922f5e41c9e210a0a52e4e8af6776970bff1
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-05-28 08:35:18 -0700

    Update the-hurdle-race.cpp

--- Algorithms/Implementation/the-hurdle-race.cpp
+++ Algorithms/Implementation/the-hurdle-race.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main() {
   int n, k;
   cin >> n >> k;
-  vector<int> h(n);
   int hmax = 0;
   for (int i = 0; i < n; i++) {
-    cin >> h[i];
-    hmax = max(hmax, h[i]);
+    int h;
+    cin >> h;
+    hmax = max(hmax, h);
   }
-  cout << max(hmax - k, 0) << "\n"; 
+  cout << max(hmax - k, 0) << endl; 
   return 0;
 }
