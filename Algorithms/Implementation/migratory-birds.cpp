e30ace48989e4401bf85693af0dc9bd114e0f752
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-06-09 02:29:31 -0400

    Update migratory-birds.cpp

--- Algorithms/Implementation/migratory-birds.cpp
+++ Algorithms/Implementation/migratory-birds.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int migratoryBirds(int n, vector<int> t) {
+int migratoryBirds(int n, vector<int>& t) {
   vector<int> cnt(5, 0);
   for (int i = 0; i < n; i++) {
     cnt[t[i] - 1]++;
   }
-  int max = -1, type;
+  int mx = -1, type = -1;
   for (int i = 0; i < 5; i++) {
-    if (cnt[i] > max) {
-      max = cnt[i];
+    if (cnt[i] > mx) {
+      mx = cnt[i];
       type = i + 1;
     }
   }
@@ -23,6 +23,6 @@ int main() {
   for (int i = 0; i < n; i++) {
     cin >> t[i];
   }
-  cout << migratoryBirds(n, t) << "\n";
+  cout << migratoryBirds(n, t) << endl;
   return 0;
 }
