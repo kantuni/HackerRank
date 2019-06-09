95d98f7c6443ace31784b134e4ddc9efa3fbab8f
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-06-09 02:30:11 -0400

    Update picking-numbers.cpp

--- Algorithms/Implementation/picking-numbers.cpp
+++ Algorithms/Implementation/picking-numbers.cpp
@@ -10,9 +10,9 @@ int main() {
     f[a[i]]++;
   }
   int cnt = 1;
-  for (int i = 0; i < n - 1; i++i) { 
+  for (int i = 0; i < n - 1; i++) { 
     cnt = max(cnt, f[i] + f[i + 1]);
   }
-  cout << cnt << "\n";
+  cout << cnt << endl;
   return 0;
 }
