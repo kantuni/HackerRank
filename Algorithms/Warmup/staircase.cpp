5d387a58680ce032033dbe7d5279d4fd7bce18af
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-06-09 02:16:32 -0400

    Update staircase.cpp

--- Algorithms/Warmup/staircase.cpp
+++ Algorithms/Warmup/staircase.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  vector<vector<char> > M(n, vector<char>(n, ' '));
+  vector<vector<char>> M(n, vector<char>(n, ' '));
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       if (i + j >= n - 1) {
